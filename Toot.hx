d9aabe8652be23f082b0c40275e28ffbf2f22168
Josu Igoa <josu.igoa@indaba.lks.es>
2022-05-02 15:04:31 +0200

    add posibility to post images

--- Toot.hx
+++ Toot.hx
@@ -1,7 +1,9 @@
 package;
 
-using api.IdeckiaApi;
+import js.node.buffer.Buffer;
+
 using StringTools;
+using api.IdeckiaApi;
 
 typedef Props = {
 	@:shared
@@ -12,12 +14,31 @@ typedef Props = {
 	var accessToken:String;
 	@:editable("Text to publish")
 	var tootText:String;
+	@:editable("Image paths", [])
+	var tootImagePaths:Array<String>;
+}
+
+typedef Attachment = {
+	// ID of the attachment
+	var id:String;
+	// One of: "image", "video", "gifv"
+	var type:String;
+	// URL of the locally hosted version of the image
+	var url:String;
+	// For remote images, the remote URL of the original image
+	var remote_url:String;
+	// URL of the preview image
+	var preview_url:String;
+	// Shorter URL for the image, for insertion into text (only present on local images)
+	var text_url:String;
 }
 
 @:name("toot")
 @:description("Publish a toot in mastodon")
 class Toot extends IdeckiaAction {
 	static inline final API_URL:String = 'api/v1';
+	static inline final STATUSES_PATH:String = 'statuses';
+	static inline final MEDIA_PATH:String = 'media';
 
 	public override function init(initialState:ItemState):js.lib.Promise<ItemState> {
 		if (!props.server.endsWith('/'))
@@ -28,23 +49,85 @@ class Toot extends IdeckiaAction {
 
 	public function execute(currentState:ItemState):js.lib.Promise<ItemState> {
 		return new js.lib.Promise((resolve, reject) -> {
-			var endpoint = props.server + '$API_URL/statuses';
+			var tootData = {
+				status: props.tootText,
+				media_ids: []
+			};
+			function toot() {
+				postStatus(tootData).then((d) -> server.log.debug("Mastodon publish response: " + d)).catchError(reject);
+			}
 
-			var http = new haxe.http.HttpNodeJs(endpoint);
+			if (props.tootImagePaths.length != 0) {
+				var mediaPromises = [];
+				for (p in props.tootImagePaths) {
+					mediaPromises.push(uploadMedia(p));
+				}
 
-			http.addHeader('Authorization', 'Bearer ' + props.accessToken);
-			http.addHeader("Content-type", "application/json");
+				js.lib.Promise.all(mediaPromises).then(attachments -> {
+					for (a in attachments)
+						tootData.media_ids.push(a.id);
 
-			http.setPostData(haxe.Json.stringify({
-				status: props.tootText
-			}));
+					toot();
+				}).catchError(reject);
+			} else {
+				toot();
+			}
 
-			http.onError = reject;
-
-			http.request(true);
-
-			server.log.debug("Mastodon publish response: " + http.responseData);
 			resolve(currentState);
 		});
+	}
+
+	function prepareRequest(path:String) {
+		var endpoint = props.server + '$API_URL/$path';
+		var http = new haxe.Http(endpoint);
+		http.addHeader('Authorization', 'Bearer ${props.accessToken}');
+		return http;
+	}
+
+	function postStatus(status:Any):js.lib.Promise<String> {
+		return new js.lib.Promise((resolve, reject) -> {
+			var http = prepareRequest(STATUSES_PATH);
+			http.addHeader('Content-Type', 'application/json');
+			http.setPostData(haxe.Json.stringify(status));
+			http.onError = reject;
+			http.onData = resolve;
+			http.request(true);
+		});
+	}
+
+	function uploadMedia(filePath:String):js.lib.Promise<Attachment> {
+		return new js.lib.Promise((resolve, reject) -> {
+			var http = prepareRequest(MEDIA_PATH);
+
+			var boundaryKey = Std.string(Math.random() * 0xFFFFFF);
+			var boundary = '--${boundaryKey}';
+			http.setHeader('Content-Type', 'multipart/form-data; boundary=' + boundary);
+
+			var multipartBody = prepareMultipartBody(filePath, boundary);
+			http.setPostBytes(multipartBody.hxToBytes());
+			http.onError = (e) -> reject('Error uploading media [$filePath]: $e.');
+			http.onData = (d) -> {
+				var attachment:Attachment = haxe.Json.parse(d);
+				server.log.debug('Uploaded attachment: ${attachment.id}');
+				resolve(attachment);
+			}
+			http.request(true);
+		});
+	}
+
+	function prepareMultipartBody(filePath:String, boundary:String) {
+		var data = js.node.Fs.readFileSync(filePath);
+
+		var crlf = "\r\n";
+		var delimeter = '${crlf}--${boundary}';
+		var headers = [
+			'Content-Disposition: form-data; name="file"; filename="${haxe.io.Path.withoutDirectory(filePath)}"' + crlf
+		];
+		var closeDelimeter = '${delimeter}--';
+		return Buffer.concat([
+			Buffer.from(delimeter + crlf + headers.join('') + crlf),
+			data,
+			Buffer.from(closeDelimeter)
+		]);
 	}
 }
--- readme.md
+++ readme.md
@@ -11,6 +11,7 @@ Publish a predefined text to a mastodon instance
 | server | String | Server of the Mastodon instance | true | "https://mastodon.social" | null |
 | accessToken | String | Access token value | true | null | null |
 | tootText | String | Text to publish | false | null | null |
+| tootImagePaths | Array&lt;String&gt; | Image paths | false | [] | null |
 
 ## On single click
 
@@ -31,7 +32,10 @@ Does nothing
             "props": {
                 "server": "https://mastodon.social",
                 "accessToken": null,
-                "tootText": null
+                "tootText": null,
+                "tootImagePaths": [
+                    "/path/to/image.jpg"
+                ]
             }
         }
     ]
@@ -44,5 +48,6 @@ Does nothing
 * Go to Settings (the gear icon) -> Development -> New application
   * Give the application a descriptive name
   * Check the "write:statuses" checkbox
+  * If you want to publish images, check the "write:media" checkbox too
   * Click SUBMIT
 * Click in the newly created application and there is "Your access token" that you need.
